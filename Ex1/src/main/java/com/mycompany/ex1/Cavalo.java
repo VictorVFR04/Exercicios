d69a845e64984676ce1154d049207d02533fdca5
Victor F. Ramos <120141481+VictorVFR04@users.noreply.github.com>
2023-03-08 20:33:54 -0300

    Add files via upload

---
+++ Ex1/src/main/java/com/mycompany/ex1/Cavalo.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.ex1;
+
+/**
+ *
+ * @author aluno
+ */
+public class Cavalo {
+    
+    public String nome;
+    public int peso;
+    public String olhos;
+    public int velocidade;
+    public String corPelo;
+}
---
+++ Ex1/src/main/java/com/mycompany/ex1/Ex1.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.ex1;
+
+/**
+ *
+ * @author Victor Fagundes Ramos
+ */
+public class Ex1 {
+
+    public static void main(String[] args) {
+        
+    Cavalo cavalo1 = new Cavalo();
+    cavalo1.nome = "Juan";
+    cavalo1.peso = 90;
+    cavalo1.olhos = "castanho";
+    cavalo1.velocidade = 4;
+    cavalo1.corPelo = "preto";
+    
+    
+    Cavalo cavalo2 = new Cavalo();
+    cavalo2.nome = "Vinicius";
+    cavalo2.peso = 87;
+    cavalo2.olhos = "castanho";
+    cavalo2.velocidade = 10;
+    cavalo2.corPelo = "marrom";
+    
+    
+    Cavalo cavalo3 = new Cavalo();
+    cavalo3.nome = "Marcos";
+    cavalo3.peso = 54;
+    cavalo3.olhos = "castanho";
+    cavalo3.velocidade = 9;
+    cavalo3.corPelo = "castanho";
+    
+    
+    Cavalo cavalo4 = new Cavalo();
+    cavalo4.nome = "Lucas";
+    cavalo4.peso = 65;
+    cavalo4.olhos = "castanho";
+    cavalo4.velocidade = 7;
+    cavalo4.corPelo = "preto";
+    
+    
+    Cavalo cavalo5 = new Cavalo();
+    cavalo5.nome = "Matheus";
+    cavalo5.peso = 53;
+    cavalo5.olhos = "castanho";
+    cavalo5.velocidade = 8;
+    cavalo5.corPelo = "branco";
+    }
+}
