82d2bcdc38f2749035f46c04e8d1af2ee3658d4b
adrianlucastdk <adrianlucastoderke@hotmail.com>
2023-03-14 20:35:29 -0300

    importacao

---
+++ Construtor/src/main/java/com/mycompany/construtor/ConstrutorCavalos.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package com.mycompany.construtor;
+
+/**
+ *
+ * @author aluno
+ */
+public class ConstrutorCavalos {
+    
+    public static void main(String[] args) {
+        Construtor cavalo = new Construtor("Juan");
+        System.out.println(cavalo.getNome());
+    }
+
+    
+    }
+
